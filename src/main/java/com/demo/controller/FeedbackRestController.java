b26a9761115a8784ee9736f492d160b28ddbc98f
sharkchili6868 <40523036+sharkchili6868@users.noreply.github.com>
2020-06-23 12:47:31 -0700

    add comments to rest controller

--- src/main/java/com/demo/controller/FeedbackRestController.java
+++ src/main/java/com/demo/controller/FeedbackRestController.java
@@ -26,7 +26,7 @@ public class FeedbackRestController {
 
     /**
      *
-     * @param id -> primary
+     * @param id -> primary key of feedback object in database
      * @return
      */
     @GetMapping("/feedback/{id}")
@@ -34,6 +34,10 @@ public class FeedbackRestController {
         return feedbackService.listFeedbackById(id);
     }
 
+    /**
+     *
+     * @param feedback -> json body of feedback that you want to post
+     */
     @PostMapping("/feedback")
     public void createNewFeedback(@RequestBody Feedback feedback) {
         Date now = new Date();
@@ -41,16 +45,31 @@ public class FeedbackRestController {
         feedbackService.createNewFeedback(feedback);
     }
 
+    /**
+     *
+     * @param feedback -> json body of feedback that you want to edit
+     * @param id -> primary key of feedback object in database
+     */
     @PutMapping("/feedback/{id}")
     public void editFeedbackById(@RequestBody Feedback feedback, @PathVariable int id) {
         feedbackService.editFeedbackById(feedback, id);
     }
 
+    /**
+     *
+     * @param evaluator -> name of evaluator
+     * @return -> all feedback object as an array under this evaluator
+     */
     @GetMapping("/feedback/evaluator")
     public List<Feedback> listAllFeedbackByEvaluator(@RequestParam String evaluator) {
         return feedbackService.listAllFeedbackByEvaluator(evaluator);
     }
 
+    /**
+     *
+     * @param interviewee -> name of interviewee
+     * @return -> all feedback object as an array under this interviewee
+     */
     @GetMapping("/feedback/interviewee")
     public List<Feedback> listAllFeedbackByInterviewee(@RequestParam String interviewee) {
         return feedbackService.listAllFeedbackByEvaluator(interviewee);
